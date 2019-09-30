61122064cf6d4a2b092ca5862261425f397ee9dc
jannick holm <jannickholm@desktop-n6ddcg0.kea.local>
2019-09-30 13:06:53 +0200

    is.js test

---
+++ index.js
@@ -0,0 +1,8 @@
+import is from "is_js";
+'use strict'
+document.addEventListener('DOMContentLoaded', start);
+console.log(is.chrome());
+
+function start() {
+    console.log('Connection between the DOM and the Script was successfull! Nice 😎');
+}
